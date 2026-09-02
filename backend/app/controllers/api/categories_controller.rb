616d5a7577ba57484740f4962026cf0a0d7c7651
rondale-bufete <rondale.bufete7@gmail.com>
2026-09-02 15:56:18 +0800

    feat(category): add update and destroy actions

--- backend/app/controllers/api/categories_controller.rb
+++ backend/app/controllers/api/categories_controller.rb
@@ -14,6 +14,28 @@ class Api::CategoriesController < ApplicationController
     end
   end
 
+  def update
+    category = Category.find(params[:id])
+    
+    if category.update(category_params)
+      render json: category
+    else
+      render json: { errors: category.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    category = Category.find(params[:id])
+
+    if category.expenses.any?
+      expense_count = category.expenses.count
+      render json: { errors: ["Cannot delete category with #{expense_count} expenses"] }, status: :unprocessable_entity
+    else
+      category.destroy
+      render status: :no_content
+    end
+  end
+
   private
 
   def category_params
